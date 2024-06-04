9bbe5e48ceb521e94c3c1ecc526f7ddd31d0d7d2
Tushika <tushikagarg11@gmail.com>
2024-06-04 17:52:53 +0530

    add meals and database

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,15 @@
+import Link from "next/link";
+import classes from './page.module.css'
+import MealsGrid from "@/components/meals/meals-grid";
 export default function MealsPage(){
     return(
-        <h1>Meals</h1>
+        <>
+            <header className={classes.header}>
+                <h1>Delicious Meals, created{' '}<span className={classes.highlight}>by you</span></h1>
+                <p>Choose your favourite recipe and cook it yourself. It is easy and fun!</p>
+                <p className={classes.cta}><Link href="/meals/share">Share your favourite recipe</Link></p>
+            </header>
+            <main className={classes.main}><MealsGrid meals={[]}/></main>
+        </>
     )
 }
